45625d504ac121966f136f998ba89f7ea909159b
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-12-01 15:35:58 +0100

    Update Carousel component styling

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -2,16 +2,20 @@ import images from "../data/store";
 
 export default function Carousel() {
   return (
-    <div className="flex flex-row max-w-[1200px] bg-red-400">
-      <button>Prev</button>
+    <div className="flex items-center gap-6 max-w-[1200px]">
+      <button className="p-4 bg-blue-500">Prev</button>
 
-      <div className="">
+      <div className="relative">
         <div className="">
           <img src={images[0]} alt="" />
         </div>
+        <div className="p-4 absolute bottom-0 text-white w-full bg-[rgba(80,80,80,0.5)]">
+          <h1>Primo Post</h1>
+        </div>
       </div>
 
-      <button>Next</button>
+      <button className="p-4 bg-blue-500">Next</button>
+
     </div>
   );
 }
