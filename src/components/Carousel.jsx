6163fa6626b750a809731b2831d893c45ceb5833
giacomopaolini1 <giacomopaolini08@gmail.com>
2023-12-01 16:06:36 +0100

    Adds carousel functionality

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,21 +1,40 @@
+import { useState } from "react";
 import images from "../data/store";
 
 export default function Carousel() {
+
+  const [currentImage, setCurrentImage] = useState(0);
+
+  function handleNext() {
+    setCurrentImage((currentImage + 1) % images.length);
+  }
+
+  function handlePrev() {
+    setCurrentImage((currentImage - 1 + images.length) % images.length);
+  }
+
   return (
     <div className="flex items-center gap-6 max-w-[1200px]">
-      <button className="p-4 bg-blue-500">Prev</button>
+      <button onClick={handlePrev} className="p-4 bg-red-500 rounded-full">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
+      </svg>
+      </button>
 
       <div className="relative">
-        <div className="">
-          <img src={images[0]} alt="" />
+        <div>
+          <img src={images[currentImage].image} alt="" />
         </div>
         <div className="p-4 absolute bottom-0 text-white w-full bg-[rgba(80,80,80,0.5)]">
-          <h1>Primo Post</h1>
+          <h1>{images[currentImage].title}</h1>
         </div>
       </div>
 
-      <button className="p-4 bg-blue-500">Next</button>
-
+      <button onClick={handleNext} className="p-4 bg-red-500 rounded-full">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
+      </svg>
+      </button>
     </div>
   );
 }
--- src/data/store.js
+++ src/data/store.js
@@ -1,10 +1,33 @@
 const images = [
-    "https://picsum.photos/id/10/600/400",
-    "https://picsum.photos/id/11/600/400",
-    "https://picsum.photos/id/12/600/400",
-    "https://picsum.photos/id/13/600/400",
-    "https://picsum.photos/id/14/600/400",
-    "https://picsum.photos/id/15/600/400",
+    {
+        title: "Post 1",
+        image: "https://picsum.photos/id/10/600/400",
+    }
+    ,
+    {
+        title: "Post 2",
+        image: "https://picsum.photos/id/11/600/400",
+    }
+    ,
+    {
+        title: "Post 3",
+        image: "https://picsum.photos/id/12/600/400",
+    }
+    ,
+    {
+        title: "Post 4",
+        image: "https://picsum.photos/id/13/600/400",
+    }
+    ,
+    {
+        title: "Post 5",
+        image: "https://picsum.photos/id/14/600/400",
+    }
+    ,
+    {
+        title: "Post 6",
+        image: "https://picsum.photos/id/15/600/400",
+    }
 ]
 
 export default images;
